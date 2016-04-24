40c17410f22ac53e2aeab41dbd55041dbac277bd
Robin Clowers <robin.clowers@gmail.com>
2016-04-23 20:47:32 -0700

    Add score UI

--- lib/game.rb
+++ lib/game.rb
@@ -17,6 +17,7 @@ class GameWindow < Gosu::Window
     @player = Player.new(Height / 2, Width / 2)
     @star_animation = Image.load_tiles("media/star.png", 25, 25)
     @stars = []
+    @font = Font.new(20)
   end
 
   def update
@@ -35,6 +36,7 @@ class GameWindow < Gosu::Window
     @background_image.draw(0, 0, ZIndex::Background)
     @player.draw
     @stars.each(&:draw)
+    @font.draw("Score: #{@player.score}", 10, 10, ZIndex::UI, 1.0, 1.0, 0xff_ffff00)
   end
 end
 
--- lib/z_index.rb
+++ lib/z_index.rb
@@ -2,4 +2,5 @@ module ZIndex
   Background = 0
   Stars = 1
   Player = 2
+  UI = 3
 end
